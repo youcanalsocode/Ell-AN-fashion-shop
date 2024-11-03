b45f2846e6a3fff6dc8962de7dd5eb58c1baefd4
youcanalsocode <nehan.gupta12@gmail.com>
2024-11-03 14:09:29 +0530

    Navbar color and popup fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Popover, OverlayTrigger } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../assets/style/Navbar.css";
 
-
 const Navbar = () => {
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>
+        <Link className="dropdown-item" to="/signin">
+          Signin
+        </Link>
+        <Link className="dropdown-item" to="/logout">
+          Logout
+        </Link>
+        <Link className="dropdown-item" to="/wishlist">
+          Wishlist
+        </Link>
+      </Popover.Body>
+    </Popover>
+  );
+
   return (
-    <header>
+    <header >
       {/* First Navbar */}
-      <nav className="navbar navbar-expand-lg bg-body-tertiary border-bottom border-body">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary border-bottom border-body navbar1" >
         <div className="container-fluid d-flex justify-content-between align-items-center">
-          <a className="navbar-brand" href="#" >
+          <a className="navbar-brand" href="#">
             <img
               src={require("../assets/Images/brand-logo.png")}
               height="20px"
@@ -17,20 +33,17 @@ const Navbar = () => {
               className="brand-logo"
               alt="Logo"
             />
-        {/* <span className="brand-name">ell&an</span> */}
           </a>
 
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0 d-flex flex-row flex-nowrap">
             <li className="nav-item">
               <Link className="nav-link active" aria-current="page" to="/">
-              <i class="fa-solid fa-house"></i>
+                <i className="fa-solid fa-house"></i>
               </Link>
             </li>
-         
             <li className="nav-item">
               <Link className="nav-link active" aria-disabled="true" to="#">
-              <i class="fa-solid fa-cart-shopping"></i>
-                
+                <i className="fa-solid fa-cart-shopping"></i>
               </Link>
             </li>
             <li className="nav-item">
@@ -38,43 +51,19 @@ const Navbar = () => {
                 About
               </Link>
             </li>
-            <li className="nav-item dropdown">
-              <Link
-                className="nav-link dropdown-toggle"
-                to="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                <i class="fa-solid fa-user"></i>
-              </Link>
-              <ul className="dropdown-menu">
-                <li>
-                  <Link className="dropdown-item" to="/signin">
-                    Signin
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/logout">
-                    Logout
-                  </Link>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/wishlist">
-                    Wishlist
-                  </Link>
-                </li>
-              </ul>
+            <li className="nav-item">
+              <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
+                <button className="nav-link" type="button">
+                  <i className="fa-solid fa-user"></i>
+                </button>
+              </OverlayTrigger>
             </li>
           </ul>
         </div>
       </nav>
 
-      {/* Second Navbar with Search Bar */}
-      <nav className="navbar navbar-light bg-light">
+    
+      <nav className="navbar  navbar-light  navbar2">
         <div className="container-fluid d-flex justify-content-between align-items-center">
           <form className="d-flex align-items-center w-100" role="search">
             <input
@@ -87,7 +76,7 @@ const Navbar = () => {
               Search
             </button>
             <button className="btn btn-outline" type="button">
-              <i className="bi bi-search "></i> {/* Bootstrap icon for search */}
+              <i className="bi bi-search "></i> 
             </button>
           </form>
         </div>
